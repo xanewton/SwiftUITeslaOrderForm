c7e87161f3d6bc0c70076fd5b3ab35f2ec41e0e7
Angel Newton <xanewton@gmail.com>
2021-02-18 19:27:36 -0500

    Initial Commit

---
+++ SwiftUITeslaOrderForm/SwiftUITeslaOrderFormApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITeslaOrderFormApp.swift
+//  SwiftUITeslaOrderForm
+//
+//  Created by newtan on 2021-02-18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITeslaOrderFormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
