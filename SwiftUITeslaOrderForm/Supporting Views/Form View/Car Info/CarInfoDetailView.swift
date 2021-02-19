3953eca59350ead7774afe7c76ad913c621c1cef
Angel Newton <xanewton@gmail.com>
2021-02-18 20:03:46 -0500

    Create CarInfoDetailView

--- SwiftUITeslaOrderForm/Supporting Views/Form View/Car Info/CarInfoDetailView.swift
+++ SwiftUITeslaOrderForm/Supporting Views/Form View/Car Info/CarInfoDetailView.swift
@@ -10,12 +10,49 @@ import SwiftUI
 
 struct CarInfoDetailView: View {
     var body: some View {
-        Text("Car Info Detail View")
+        HStack {
+            column1
+            Spacer()
+            column2
+            Spacer()
+            column3
+        }.padding(.top, 15)
+    }
+    
+    var column1: some View {
+        VStack(alignment: .leading) {
+            Text("Transmission")
+                .custom(font: .medium, size: 16)
+                .foregroundColor(.baseDarkGray)
+            Text("Automatic")
+                .custom(font: .medium, size: 22)
+        }
+    }
+
+    var column2: some View {
+        VStack(alignment: .leading) {
+            Text("Class")
+                .custom(font: .medium, size: 16)
+                .foregroundColor(.baseDarkGray)
+            Text("Luxury")
+                .custom(font: .medium, size: 22)
+        }
+    }
+
+    var column3: some View {
+        VStack(alignment: .leading) {
+            Text("Year")
+                .custom(font: .medium, size: 16)
+                .foregroundColor(.baseDarkGray)
+            Text("2020")
+                .custom(font: .medium, size: 22)
+        }
     }
 }
 
 struct CarInfoDetailView_Previews: PreviewProvider {
     static var previews: some View {
         CarInfoDetailView()
+            .previewLayout(.fixed(width: 400, height: 100))
     }
 }
