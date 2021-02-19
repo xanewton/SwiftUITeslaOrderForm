320c3a4f85cc838ce85656f96bdb8b752ef92d9b
Angel Newton <xanewton@gmail.com>
2021-02-18 20:14:44 -0500

    Create CarDetailView

--- SwiftUITeslaOrderForm/Supporting Views/Form View/CarDetailView.swift
+++ SwiftUITeslaOrderForm/Supporting Views/Form View/CarDetailView.swift
@@ -10,12 +10,28 @@ import SwiftUI
 
 struct CarDetailView: View {
     var body: some View {
-        Text("Car Detail View")
+        ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .fill(Color.baseGray)
+                .frame(height: 442)
+            VStack(spacing: 10) {
+                VStack {
+                    Image("tesla-logo").offset(y: 120)
+                    Image("tesla-s")
+                    Image("tesla-text-logo").offset(y: -10)
+                }
+                CarInfoView()
+            }.offset(y: -155)
+        }
+        .padding(.horizontal, 12)
+        .offset(y: 100)
+        .frame(height: 250)
     }
 }
 
 struct CarDetailView_Previews: PreviewProvider {
     static var previews: some View {
         CarDetailView()
+            .previewLayout(.fixed(width: 400, height: 650))
     }
 }
