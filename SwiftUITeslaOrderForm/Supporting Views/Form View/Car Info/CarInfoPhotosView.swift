390938aa5370ae060a3590e580e06b98226e6a5c
Angel Newton <xanewton@gmail.com>
2021-02-18 20:08:12 -0500

    Create CarInfoPhotosView

--- SwiftUITeslaOrderForm/Supporting Views/Form View/Car Info/CarInfoPhotosView.swift
+++ SwiftUITeslaOrderForm/Supporting Views/Form View/Car Info/CarInfoPhotosView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 
 struct CarInfoPhotosView: View {
     var body: some View {
-        Text("Car Info Photos View")
+        VStack(alignment: .leading, spacing: 4) {
+            Text("Car Interior")
+                .custom(font: .medium, size: 22)
+                .foregroundColor(.baseDarkGray)
+            HStack {
+                Image("pic1")
+                    .resizable()
+                    .frame(width: 170, height: 94)
+                    .cornerRadius(10)
+                Image("pic2")
+                    .resizable()
+                    .frame(width: 170, height: 94)
+                    .cornerRadius(10)
+            }.frame(height: 94)
+        }.padding(.top, 10)
     }
 }
 
 struct CarInfoPhotosView_Previews: PreviewProvider {
     static var previews: some View {
         CarInfoPhotosView()
+            .previewLayout(.fixed(width: 400, height: 150))
     }
 }
