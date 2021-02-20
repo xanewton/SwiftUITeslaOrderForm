2710bb1a39628c923093564c279df5ff916cb94d
Angel Newton <xanewton@gmail.com>
2021-02-19 19:51:06 -0500

    Show order form in ConteView

--- SwiftUITeslaOrderForm/Views/ContentView.swift
+++ SwiftUITeslaOrderForm/Views/ContentView.swift
@@ -8,9 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    //@EnvironmentObject var order:OrderViewModel
+    @ObservedObject var order = OrderViewModel()
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            NavigationView {
+                ScrollView(.vertical) {
+                    VStack(spacing: 0) {
+                        CarDetailView()
+                            .frame(height: 600)
+                        FormView()
+                            .environmentObject(order)
+                            .frame(height: 450)
+                    }
+                    .padding(.top, 40)
+                }
+                .hideNavigationBar()
+            }
+            
+            CompleteOrderView()
+                .environmentObject(order)
+                .opacity(order.isOrderCompleteVisible ? 1 : 0)
+                .animation(.default)
+        }
     }
 }
 
