0293cde890cddabffa15cb36992af79f6221a16c
Angel Newton <xanewton@gmail.com>
2021-02-19 20:03:05 -0500

    Create CancelOrderView

--- SwiftUITeslaOrderForm/Views/Cancel Order View/CancelOrderView.swift
+++ SwiftUITeslaOrderForm/Views/Cancel Order View/CancelOrderView.swift
@@ -9,8 +9,36 @@
 import SwiftUI
 
 struct CancelOrderView: View {
+    //@EnvironmentObject var order:OrderViewModel
+    @ObservedObject var order = OrderViewModel()
+
     var body: some View {
-        Text("Cancel Order View")
+        ZStack {
+            Rectangle()
+                .fill(Color.clear)
+                .background(VibrancyBackground())
+                .edgesIgnoringSafeArea(.all)
+
+            ZStack {
+                Rectangle()
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 190)
+                    .background(Color.black)
+                    .cornerRadius(10)
+                    .opacity(0.2)
+                    .blur(radius: 10)
+
+                CancelModalView()
+            }
+            .padding(.horizontal, 10)
+            .animation(.default)
+        }
+        .onAppear {
+            self.order.isModalVisible = true
+        }.onTapGesture {
+            self.order.isCancelOrderVisible.toggle()
+            self.order.isModalVisible.toggle()
+        }
     }
 }
 
