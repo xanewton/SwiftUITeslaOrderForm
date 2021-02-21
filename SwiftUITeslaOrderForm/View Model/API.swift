ac49892ceb4b45aeeea72055d9604b468c0f038f
Angel Newton <xanewton@gmail.com>
2021-02-21 17:41:49 -0500

    Add Network with Combine

--- SwiftUITeslaOrderForm/View Model/API.swift
+++ SwiftUITeslaOrderForm/View Model/API.swift
@@ -8,3 +8,63 @@
 
 import Foundation
 import Combine
+
+struct API {
+
+    enum Error: LocalizedError, Identifiable {
+        var id: String { localizedDescription }
+        
+        case addressUnreachable(URL)
+        case invalidResponse
+        
+        var errorDescription: String? {
+            switch self {
+                case .invalidResponse:
+                    return "The server responded with garbage."
+                case .addressUnreachable(let url):
+                    return "\(url.absoluteString)"
+            }
+        }
+    }
+
+    enum EndPoint {
+        static let base = URL(string: "https://reqres.in/")!
+        case post
+        var url: URL {
+            switch self {
+            case .post:
+                return EndPoint.base.appendingPathComponent("api/tesla-order")
+            }
+        }
+        
+        static func request(with url: URL, and order:OrderViewModel) -> URLRequest {
+            guard let encoded = try? JSONEncoder().encode(order) else {
+                fatalError("Invalid")
+            }
+            var request = URLRequest(url: url)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            request.httpMethod = "POST"
+            request.httpBody = encoded
+            return request
+        }
+    }
+    
+    private let decoder = JSONDecoder()
+    
+    func post(with order: OrderViewModel) -> AnyPublisher<OrderViewModel, Error> {
+        URLSession.shared.dataTaskPublisher(for: EndPoint.request(with: EndPoint.post.url, and: order))
+            .map { $0.data }
+            .decode(type: OrderViewModel.self, decoder: decoder)
+            .mapError { error in
+                switch error {
+                    case is URLError:
+                        return Error.addressUnreachable(EndPoint.post.url)
+                    default:
+                        return Error.invalidResponse
+                }
+            }
+            .print()
+            .map { return $0 }
+            .eraseToAnyPublisher()
+    }
+}
